08d529b68264bbf0993e41f5a68567b794472f95
warheim <warheim24@gmail.com>
2024-09-02 16:14:18 +0300

    searching diffs with vscode

--- download_archive/download_file.py
+++ download_archive/download_file.py
@@ -1,4 +1,3 @@
-import time
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 
@@ -10,33 +9,16 @@ class DownloadArchive:  # TODO The changed design! Re-load!
         self.file_name = file_name
 
         self.chrome_options = Options()
-        self.chrome_options.add_argument("--headless")
+        # self.chrome_options.add_argument("--headless")
+        self.chrome_options.add_argument('--no-sandbox')
         self.chrome_options.add_argument("--start-maximized")
 
         self.selenium_driver = Chrome(options=self.chrome_options)
 
     def page_download(self):
         self.selenium_driver.get(self.link)
-        old_position = 0
-        new_position = None
-        while new_position != old_position:
-            # Get old scroll position
-            old_position = self.selenium_driver.execute_script(
-                ("return (window.pageYOffset !== undefined) ?"
-                 " window.pageYOffset : (document.documentElement ||"
-                 " document.body.parentNode || document.body);"))
-            # Sleep and Scroll
-            time.sleep(1)
-            self.selenium_driver.execute_script((
-                "var scrollingElement = (document.scrollingElement ||"
-                " document.body);scrollingElement.scrollTop ="
-                " scrollingElement.scrollHeight;"))
-            # Get new position
-            new_position = self.selenium_driver.execute_script(
-                ("return (window.pageYOffset !== undefined) ?"
-                 " window.pageYOffset : (document.documentElement ||"
-                 " document.body.parentNode || document.body);"))
-
+        self.selenium_driver.implicitly_wait(3)
+        cookie = self.selenium_driver.find_element(by=)
         with open(self.file_name, "w", encoding=self.encoding) as file:
             file.write(self.selenium_driver.page_source)
         self.selenium_driver.quit()
